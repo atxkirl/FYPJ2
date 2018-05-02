3126c2817451dcdf2a48df9c1c0a568e60e101cf
ATXK-WSPC <atxkirl@gmail.com>
2018-05-02 21:34:39 +0800

    Changelog: - Added GameManager script (currently does nothing) - Updated gitignore file

---
+++ FYPJ2_Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public List<Character> CharacterList = new List<Character>();
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
+
+public class Character
+{
+	public string name;
+	public Texture2D previewImage;
+	public GameObject playerPrefab;
+}
