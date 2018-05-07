7f0842aa6acecf9f3dc701116a56950cba16ebda
ATXK-WSPC <atxkirl@gmail.com>
2018-05-07 22:02:30 +0800

    Added inventory stuff

---
+++ FYPJ2_Unity/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <Item>
+/// This is a base class for inventory items
+/// </Item>
+
+public class Item : MonoBehaviour
+{
+	public string itemID;
+	public string itemDisplayName;
+	public string itemDisplayDescription;
+	public int itemWeight;
+	public GameObject itemPrefab;
+}
